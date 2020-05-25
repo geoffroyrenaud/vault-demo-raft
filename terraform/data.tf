5b59799e73825dcc530292aa3455794da756448f
Geoffroy RENAUD <geoffroy.renaud@accor.com>
2020-05-25 18:26:43 +0200

    add AWS terraform demo cluster

---
+++ terraform/data.tf
@@ -0,0 +1,23 @@
+data "aws_ami" latest_amzn_linux_2 {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
---
+++ terraform/main.tf
@@ -0,0 +1,74 @@
+provider "aws" {
+  region = var.region
+}
+
+
+resource "tls_private_key" vault {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" vault {
+  key_name   = var.myname
+  public_key = tls_private_key.vault.public_key_openssh
+}
+
+resource "aws_security_group" "vault" {
+  name        = var.myname
+  description = "Allow SSH + Vault inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "ssh from mycidr"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.mycidr
+  }
+
+  ingress {
+    description = "vault service from mycidr"
+    from_port   = 8200
+    to_port     = 8200
+    protocol    = "tcp"
+    cidr_blocks = var.mycidr
+  }
+
+  ingress {
+    description = "vault services between vault"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    self        = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.myname
+  }
+}
+
+module "ec2_cluster" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 2.0"
+
+  name           = var.myname
+  instance_count = 3
+
+  ami                    = data.aws_ami.latest_amzn_linux_2.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.vault.key_name
+  vpc_security_group_ids = [aws_security_group.vault.id]
+  subnet_ids             = data.aws_subnet_ids.default.ids
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
